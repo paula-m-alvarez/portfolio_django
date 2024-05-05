ec89759d415d2df98ac1905fd4665f7a0fa303a2
paula-m-alvarez <alvarez.paula.maria@gmail.com>
2024-05-05 20:08:03 -0300

    Changes in post detail page

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ def render_posts(request):
     return render(request, 'posts.html', {'posts' : posts})
 
 def post_detail(request, post_id):
-    get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {"post" : post_id})
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post_detail.html', {"post" : post})
